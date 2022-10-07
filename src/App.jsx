72e94bfcbf580bc57f406498615deaaf4ec5f085
Wiltonpatrocino <77906157+Wiltonpatrocino@users.noreply.github.com>
2022-10-07 18:24:10 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,39 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Product from './pages/Product'
-import Login from './pages/Login'
-import Purchases from './pages/Purchases'
-import './App.css'
-import MyNav from './components/MyNav'
-import Loading from './components/Loading'
-import {useDispatch, useSelector} from "react-redux"
-import { useEffect } from 'react'
-import {getProductsThunk} from './store/slices/products.slice'
-import ProtectedRoutes from './components/ProtectedRoutes'
+
+import './App.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { Home, Product, Purchases, Login, SignUp, UserInfo } from './pages';
+import { Footer, NavBar, LoadingScreen } from './components';
+import { useSelector } from 'react-redux';
+import ProtectedRoutes from 'components/ProtectedRoutes';
 
 
 function App() {
-  
-  const isLoading = useSelector(state => state.isLoading)
-
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    dispatch(getProductsThunk())
-  }, [])
+  const [count, setCount] = useState(0)
 
   return (
-   <HashRouter>
-    <MyNav/>
-    {isLoading && <Loading/>}
+    <HashRouter>
+
+    { isLoading && <LoadingScreen /> }
+
+    <NavBar />
+    <div className="content">
+
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/product/:id" element={<Product/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route element={<ProtectedRoutes/>}>
-          <Route path="/purchases" element={<Purchases/>}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+
+        <Route element={<ProtectedRoutes />}>
+          <Route path="/purchases" element={<Purchases />} />
+          <Route path="/user" element={<UserInfo />} />
         </Route>
+
       </Routes>
-   </HashRouter>
+      
+    </div>
+    <Footer />
+  </HashRouter>
   )
 }
 
